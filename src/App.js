81b603550e829a40dee4e132eb5919104493ec94
Vitalik0001 <vitalik.n1803@gmail.com>
2023-04-04 12:50:32 +0300

    update index file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import Start from "./components/Start/index.js";
+import Profile from "./components/Profile/index.js";
 import Main from "./components/Main/index.js";
 
 export default function App() {
   return (
-    <Main typeOfTip='Hydration'
-      subtitleTip='Consuming fruit juices keeps up the hydration level.'
-      userName='ViMADI'
-
-    />
+    <>
+      <Start />
+      <Profile />
+      <Main 
+        typeOfTip='Hydration'
+        subtitleTip='Consuming fruit juices keeps up the hydration level.'
+        userName='ViMADI'
+      />
+    </>
   )
 }
