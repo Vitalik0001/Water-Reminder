38f3ff8e252185ba19fc7bcaab0ff961e7e6a1bf
Vitalik0001 <vitalik.n1803@gmail.com>
2023-04-06 10:17:08 +0300

    change components

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,7 +1,5 @@
 import styles from "./index.module.scss";
 import Dashboard from "./Dashboard/index";
-import Intake from "./Intake/index";
-import Tip from "./Tip/index";
 import WaterLog from "./WaterLog/index";
 
 // imgs
@@ -12,15 +10,10 @@ import dashboard from "../../assets/img/main/dashboard.svg";
 import burger from "../../assets/img/main/burger.svg";
 import setting from "../../assets/img/main/setting.svg";
 import avatar from "../../assets/img/main/7342052.jpg";
-import intake1 from '../../assets/img/main/daily-intake-1.svg';
-import intake2 from '../../assets/img/main/daily-intake-2.svg';
-import intake3 from '../../assets/img/main/daily-intake-3.svg';
 import calendar from "../../assets/img/main/calendar.svg";
 import bottle from "../../assets/img/main/water-bottle.svg";
 import bottleBackground from "../../assets/img/main/water-bottle-bg.svg";
-import watermelon from "../../assets/img/main/watermelon.svg";
-import oranges from "../../assets/img/main/oranges.svg";
-import grapes from "../../assets/img/main/grapes.svg";
+
 
 
 const Main = ({ typeOfTip, subtitleTip, userName }) => {
@@ -66,38 +59,11 @@ const Main = ({ typeOfTip, subtitleTip, userName }) => {
               <span className={styles.userName}>{userName}!</span>
             </div>
           </div>
-          <div className={styles.weather}></div>
-          <div className={styles.intakes}>
-            <Intake nameElement="Daily" amount="5000" numberElement="1" imgForEl={intake1} />
-            <Intake nameElement="Average" amount="2500" numberElement="2" imgForEl={intake2} />
-            <Intake nameElement="Total" amount="17000" numberElement="3" imgForEl={intake3} />
-          </div>
-          <div className={styles.currentChosenMenuLink}>
-            <Dashboard />
-          </div>
-          <div className={styles.tipsBoard}>
-            {typeOfTip} Tips
-            <span className={styles.subtitleTip}>{subtitleTip} </span>
-            <div className={styles.tipsRow}>
-              <Tip
-                numberElement="1"
-                tipName="Watermelon"
-                tipText="It contains 97% water in it. A good choice to stay hydrated."
-                src={watermelon}
-              />
-              <Tip
-                number="2"
-                tipName="Oranges"
-                tipText="It contains 72% water in it. It’s tangy nature helps with skin care."
-                src={oranges}
-              />
-              <Tip
-                number="3"
-                tipName="Grapes"
-                tipText="It contains vitamin ‘C’ which helps with retaining water."
-                src={grapes}
-              />
-            </div>
+          <div>
+            <Dashboard 
+              typeOfTip='Hydration'
+              subtitleTip='Consuming fruit juices keeps up the hydration level.'
+            />
           </div>
         </div>
         <div className={styles.rightColumn}>
